7eacbdbb4a998742a26b963933505ab2a483f681
PieroB-agrbnk <piero.best.co@gmail.com>
2026-04-14 11:10:36 -0500

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -55,6 +55,7 @@ COLEGIOS_LIMA = [
     "Pamer Surco",
     "Trilce Surco",
     "Saco Oliveros Surco",
+    "Pio XII"
     # --- San Borja ---
     "San Ignacio de Recalde",
     "De La Inmaculada",
